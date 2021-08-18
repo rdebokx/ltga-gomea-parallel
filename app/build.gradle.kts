dd6e9ac01d50ecb41d223bd6103874bc0e37046d
Roy de Bokx <rdebokx1990@gmail.com>
2021-08-18 13:16:36 +0200

    Update Readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,5 +26,5 @@ dependencies {
 
 application {
     // Define the main class for the application.
-    mainClass.set("com.rdebokx.ltga.App")
+    mainClass.set("com.rdebokx.ltga.App") //TODO: update this
 }
