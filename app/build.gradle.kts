5ce486edd5a33f2713cb7fd185ade14e585d1561
Roy de Bokx <rdebokx1990@gmail.com>
2021-08-19 16:25:23 +0200

    Document parameters of sequential gradle tasks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,25 +26,67 @@ application {
 /**
  * Sequential Executables
  */
-//TODO: document these and their formats in the readme
+//TODO: point to these tasks in the readme
+//TODO: document terminology in readme
+//TODO: document per task
+
 task("RunSequentialOnemax", JavaExec::class) {
     main = "com.rdebokx.ltga.sequential.executables.Main"
-    args = listOf("ONEMAX", "10", "10", "100000", "false", "0", "0.5", "25")
+    args = listOf(
+            "ONEMAX", // Problem
+            "10", // Number of parameters
+            "10", // Population size
+            "100000", // Max number of evaluations
+            "false", // Use value to reach
+            "0", // Value to reach
+            "0.5", // Fitness value tolerance
+            "25" // Max no improvement stretch
+    )
     classpath = sourceSets["main"].runtimeClasspath
 }
 task("RunSequentialDeceptiveTrap", JavaExec::class) {
     main = "com.rdebokx.ltga.sequential.executables.Main"
-    args = listOf("DECEPTIVE_TRAP_5_TIGHT_ENCODING", "25", "5", "100000", "true", "1111111111111111111111111", "0.01", "25")
+    args = listOf(
+            "DECEPTIVE_TRAP_5_TIGHT_ENCODING", // Problem
+            "25", // Number of parameters
+            "5", // Population size
+            "100000", // Max number of evaluations
+            "false", // Use value to reach
+            "0", // Value to reach
+            "0.01", // Fitness tolerance
+            "25" // Max no improvements
+    )
     classpath = sourceSets["main"].runtimeClasspath
 }
 task("RunSequentialNkLandscapes", JavaExec::class) {
     main = "com.rdebokx.ltga.sequential.executables.Main"
-    args = listOf("NK_LANDSCAPES", "10", "10", "100000", "false", "0", "0.25", "25", "src/main/resources/problemdata/nklandscapes/N10K5S1M6_0.txt")
+    args = listOf(
+            "NK_LANDSCAPES", // Problem
+            "10", // Number of parameters. Should equal the number of parameters in the provided file.
+            "10", // Population size
+            "100000", // Max number of evaluations
+            "false", // Use value to reach
+            "0", // Value to reach
+            "0.25", // Fitness tolerance
+            "25", // Max no improvements stretch
+            "src/main/resources/problemdata/nklandscapes/N10K5S1M6_0.txt" // File with problem definition.
+    )
     classpath = sourceSets["main"].runtimeClasspath
 }
 task("RunSequentialMaxcut", JavaExec::class) {
     main = "com.rdebokx.ltga.sequential.executables.Main"
-    args = listOf("MAXCUT", "6", "10", "10000", "false", "0", "0.5", "25", "src/main/resources/problemdata/maxcut/", "n0000006i00")
+    args = listOf(
+            "MAXCUT", // Problem
+            "6", // Number of parameters. Should equal the number of parameters in the provided file.
+            "10", // Population size
+            "10000", // Max number of evaluations
+            "false",  // Use value to reach
+            "0", // Value to reach
+            "0.5", // Fitness tolerance
+            "25", // Max no improvements stretch
+            "src/main/resources/problemdata/maxcut/", // Base directory with problem definition. Should contain the provided file and matching .bkv (Best Known Value) and .arv (Average Random Value) files in the BKV and ARV folder respectively.
+            "n0000006i00" // File name of the file containing the problem definition, without .txt extension.
+    )
     classpath = sourceSets["main"].runtimeClasspath
 }
 
@@ -60,9 +102,3 @@ task("RunParallelOptimalFixedFos", JavaExec::class) {
     args = listOf("OPTIMAL_FIXED_FOS", "10", "10", "100", "false", "0", "0.5", "25")
     classpath = sourceSets["main"].runtimeClasspath
 }
-
-/**
- * Experiments
- */
-
-//TODO
